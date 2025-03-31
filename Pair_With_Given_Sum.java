9e2a8cb86b9b995a985e4aa95e47d423031dd8ed
akki2308 <95994989+akki2308@users.noreply.github.com>
2025-03-31 17:44:28 +0530

    Update Pair_With_Given_Sum.java

--- Pair_With_Given_Sum.java
+++ Pair_With_Given_Sum.java
@@ -1,47 +1,20 @@
-import java.util.*;
+import java.util.HashSet;
 
-public class Pair_With_Given_Sum {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        // Taking input
-        System.out.print("Enter the number of elements in the array: ");
-        int n = sc.nextInt();
-
-        int[] arr = new int[n];
-        System.out.println("Enter the elements of the array:");
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
-        }
-
-        System.out.print("Enter the target sum: ");
-        int target = sc.nextInt();
-
-        // Checking for a pair
-        if (!findPairWithSum(arr, n, target)) {
-            System.out.println("No pair found with the given sum.");
-        }
-
-        sc.close();
-    }
-
-    // Function to find a pair with the given sum
-    public static boolean findPairWithSum(int[] arr, int n, int target) {
-        Map<Integer, Integer> map = new HashMap<>();
-
-        for (int i = 0; i < n; i++) {
-            int complement = target - arr[i];
-
-            // Check if complement exists in the map
-            if (map.containsKey(complement)) {
-                System.out.println("Pair found: (" + complement + ", " + arr[i] + ")");
+public class PairWithSum {
+    public static boolean hasPairUsingHashSet(int[] arr, int target) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : arr) {
+            if (set.contains(target - num)) {
                 return true;
             }
-
-            // Store current number in the map
-            map.put(arr[i], i);
+            set.add(num);
         }
-
         return false;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 4, 6, 8, 10};
+        int target = 14;
+        System.out.println("Pair found: " + hasPairUsingHashSet(arr, target));
     }
 }
